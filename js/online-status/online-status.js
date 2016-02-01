e5c402e49a6ff683264ee994569fc7b342ee6acc
Ozan Kurt <me@ozankurt.com>
2016-02-01 11:02:04 +0100

    Trying stuff out

--- js/online-status/online-status.js
+++ js/online-status/online-status.js
@@ -3,59 +3,67 @@
   to use this badge on your website:
 
   1. add this A and IMG tag to your HTML
-       <a id="MY_LCTV_CHANNEL_NAME" class="lctv-badge-a">
-         <img id="lctv-online-status" class="lctv-badge-img" width="100" height="24" />
+       <a href="#">
+         <img class="lctv-badge-img" data-channel="MY_LCTV_CHANNEL" />
        </a>
   2. include this SCRIPT tag
        <script type="text/javascript" src="https://bill-auger.github.io/lctv-badges/js/online-status/online-status.js"></script>
 */
 console.log("online-status.js:IN") ;
 
-
-var BADGE_IMG_ID     = 'lctv-online-status' ;
 var LCTV_URL         = "https://www.livecoding.tv/" ;
 var LCTV_API_URL     = "https://www.livecoding.tv/livestreams/" ;
 var XHR_STATUS_READY = 4 ;
 
-var BadgeImg    = document.getElementById(BADGE_IMG_ID) ;
-var BadgeA      = BadgeImg.parentNode ;
-var ChannelName = BadgeA.id ;
-BadgeA.href     = LCTV_URL + ChannelName ;
+var badges = document.getElementsByClassName("lctv-badge-img");
+var channelName = getAttribute("data-channel");
 
-
-function getStatus()
+function getStatuses()
 {
-console.log("online-status.js:getStatus() ChannelName=" + ChannelName) ;
+    for (i = 0; i < badges.length; i++) {
+        badge = badges[i];
+        channelName = badge.getAttribute("data-channel");
+        
+        if (!badge.href) {
+            badge.href = LCTV_URL + channelName;
+        }
+        
+        console.log("Checking status of: " + channelName);
+        
+        xhr = new XMLHttpRequest();
+        
+        xhr.open("GET" , LCTV_API_URL , true);
+        xhr.setRequestHeader("Content-type" , "application/json");
+        xhr.onreadystatechange = function() { 
+            parseJSON(xhr, badge, channelName); 
+        };
+        xhr.send();
+    }
+}
+    
 // var stats_url = LCTV_API_URL + ChannelName + "/stats.json" ;
 // e.g. {"views_live": 4, "item_class": "livestream", "views_overall": 6958}
 
-  var xhr        = new XMLHttpRequest() ;
-  var status_url = LCTV_API_URL ;
-
-  xhr.open("GET" , status_url , true) ;
-  xhr.setRequestHeader("Content-type" , "application/json") ;
-  xhr.onreadystatechange = function() { parseJSON(xhr) ; } ;
-  xhr.send() ;
-}
-
-function parseJSON(xhr)
+function parseJSON(xhr, badge, channelName)
 {
-  if (xhr.readyState != XHR_STATUS_READY) return ;
+    if (xhr.readyState != XHR_STATUS_READY) 
+        return;
 
-  var is_online = !!(~xhr.responseText.indexOf("/video/livestream/" + ChannelName + "/thumbnail")) ;
+    var isOnline = !!(~xhr.responseText.indexOf("/video/livestream/" + channelName + "/thumbnail")) ;
 
-console.log("online-status.js:parseJSON() is_online=" + is_online) ;
-
-  createBadge(is_online) ;
+    createBadge(isOnline, badge, channelName) ;
 }
 
-function createBadge(is_online)
+function createBadge(isOnline, badge, channelName)
 {
-  var status_img_filename = (is_online) ? "lctv-online.png" : "lctv-offline.png" ;
-  BadgeImg.src = "../img/" + status_img_filename ;
+    statusImageFileName = (isOnline) ? "lctv-online.png" : "lctv-offline.png" ;
+    
+    badge.src = "../img/" + statusImageFileName;
 
-console.log("online-status.js:OUT") ;
+    console.log("online-status.js:OUT");
 }
 
 
-window.onload = function() { getStatus() ; } ;
+window.onload = function() { 
+    getStatus();
+};
